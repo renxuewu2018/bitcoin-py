a911cb9c8eccfe6cfb83d8cdff810509dbfdaaea
murushuo <murushuo@users.noreply.github.com>
2018-02-04 22:52:38 +0800

    add blokcchain

---
+++ bitcoin/blockchain/blockchain.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: xwren
+# @Email:  renxuewu@hotmail.com
+# @Fd:
+# @Date:   2018-02-04 22:41:47
+# @Last Modified by:   xwren
+# @Last Modified time: 2018-02-04 22:47:22
+
+class Blockchain(object):
+	def __init__(self):
+		# 存储区块链
+		self.chain= []
+		# 存储交易信息
+		self.current_transactions = []
+
+	def new_block(self):
+		pass
+
+	def hash(block):
+		pass
+
+	def new_transactions(self):
+		pass
+
+	@staticmethod
+	def hash(block):
+		pass
+
+	@property
+	def last_block(self):
+		pass
+		
