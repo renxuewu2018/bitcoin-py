5ca8f07be22e675ba8ad44330f8179bbb223a2c4
renxuewu <renxuewu@hotmail.com>
2018-03-27 17:33:18 +0800

    update

--- bitcoin/eos_analysis/ether_token_eos_transfers_chrome.py
+++ bitcoin/eos_analysis/ether_token_eos_transfers_chrome.py
@@ -6,7 +6,7 @@
 # @Fd:  spider eos transfers data every day for test
 # @Date:   2018-03-22 21:51:17
 # @Last Modified by:   renxuewu
-# @Last Modified time: 2018-03-26 23:36:53
+# @Last Modified time: 2018-03-27 17:33:02
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -17,11 +17,17 @@ from bs4  import BeautifulSoup as bs
 import time
 import mysql.connector
 import random
+from datetime import date, datetime
 from ether_token_eos_holders_top100 import token_holders_top100 as top100
+from apscheduler.schedulers.blocking import BlockingScheduler
+import pytz
+
 
 transfer_crawler = webdriver.Chrome()
 wait = WebDriverWait(transfer_crawler, 15)
 
+# 1. parser html and get transfers data
+# 2. save parser data
 def parser(url):
 	transfer_crawler.get(url)
 	# 
@@ -36,27 +42,27 @@ def parser(url):
 		else:
 			tds = tr.find_all('td')
 			TxHash = str(tds[0].string)
-			if TxHash  in txhash_list:
-				print(TxHash,'isExit')
-				continue
-				# return False
-			transfertime = get_format_time(tds[1].find('span')['title'])
+			transfertime = get_transfer_time(tds[1].find('span')['title'])
+			distime = (transfertime - last_time).total_seconds()
+			if distime < 0  :
+				return False
 			Fromaddress = str(tds[2].string)
 			InOrOut = str(tds[3].find('span').string)
 			Toaddress = str(tds[4].string)
 			Quantity = str(tds[5].string).replace(',','')
 			createtime = get_current_time()
-			print(TxHash,transfertime,Fromaddress,InOrOut,Toaddress,Quantity,createtime,'EOS','12')
 			token_holders.append((TxHash,transfertime,Fromaddress,InOrOut,Toaddress,Quantity,createtime,'EOS','12'))
-	# save_transfer(token_holders)
-	# return True
+	save_transfer(token_holders)
+	return True
 
-# conn = mysql.connector.connect(
-# 	user='root', password='imsbase', database='coin')
-# cursor = conn.cursor()
+# database
+conn = mysql.connector.connect(
+	user='root', password='imsbase', database='coin')
+cursor = conn.cursor()
 
 holder_sql = 'insert into token_transfers(TxHash,transfertime,Fromaddress,InOrOut,Toaddress,Quantity,createtime,tokenid,status)  values(%s,%s,%s,%s,%s,%s,%s,%s,%s)'
-# step 4
+
+# save tranfers data
 def save_transfer(holders):
 	cursor.executemany(holder_sql,holders)
 	conn.commit()
@@ -71,6 +77,14 @@ def isExit():
 		txhash_list.append(i[0])
 		# print(i[0])
 
+last_time = None
+# 获取最近一次更新时间
+def get_latestP_time():
+	get_current_time_sql = 'SELECT transfertime FROM token_transfers ORDER BY transfertime DESC '
+	cursor.execute(get_current_time_sql)
+	results = cursor.fetchall()
+	return results[0][0]
+
 # 关闭数据库
 def close_db():
 	cursor.close()
@@ -79,6 +93,9 @@ def close_db():
 
 def get_current_time():
 	return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+
+def get_transfer_time(t):
+	return datetime.strptime(t, '%b-%d-%Y %I:%M:%S %p')
 
 def get_format_time(t):
 	timeStruct = time.strptime(t, "%b-%d-%Y %I:%M:%S %p") 
@@ -98,18 +115,28 @@ def main():
 			page = i+1
 			next_url = url + '&p=' + str(page)
 			print(address,page)
-			parser(next_url)
-			# time.sleep(random.choice(range(1,2)))
-			# isexitstr = parser(next_url)
-			# if isexitstr:
-			# 	continue
-			# else:
-			# 	break
-			
+			# parser(next_url)
+			time.sleep(random.choice(range(1,2)))
+			isexitstr = parser(next_url)
+			if isexitstr:
+				continue
+			else:
+				break
 
-if __name__ == '__main__':
-	start = time.clock()
+def run_job():
+	tart = time.clock()
+	last_time = get_latestP_time()
 	main()
 	end = time.clock()
 	print('running time:%s seconds' %(end-start))
-	# close_db()
+
+def job():
+	print("开始执行...")
+	timez = pytz.timezone('Asia/Shanghai')
+	scheduler = BlockingScheduler(timezone=timez)
+	scheduler.add_job(func=run_job, trigger='cron',hour ='*/4')
+	scheduler.start()
+			
+
+if __name__ == '__main__':
+	job()	
